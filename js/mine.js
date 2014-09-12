c85f52fc47e9218927a59f68a5a7cc33a9c295ca
Johann Johannsson <johannjohannsson@Johanns-MacBook-Pro.local>
2014-09-12 23:56:22 +0000

    Unacceptable

---
+++ js/mine.js
@@ -0,0 +1,41 @@
+$(function() {
+  // Some animations to make lists a bit less crowded,
+  // especially in the Music Experience category
+  $(".trigger").click(function() {
+    var triggered = $(this).next();
+    if (triggered.is(":visible")) {
+      triggered.slideUp();
+    }
+    else {
+      triggered.slideDown();
+      $('html, body').animate({ 
+        scrollTop: $(this).offset().top 
+      }, 1000);
+    }
+  });
+
+  $(".trigger").mouseover(function() {
+    $(this).find(".hover_msg").fadeIn();
+  });
+
+  $(".trigger").mouseout(function() {
+    $(this).find(".hover_msg").fadeOut();
+  });
+
+  var count = 0;
+  $(".smetti").click(function() {
+    count++;
+    if (count == 3) {
+      $(".unacceptable").fadeIn().delay(1000).fadeOut();
+    }
+    else if (count == 7) {
+      $("body").css("background-color", "yellow");
+      $(".smetti").attr("src", "images/llemongrab.png");
+      $(".unacceptable").text("UNACCEPTABLE!!!");
+      for (var i = 0; i < 100; i++){
+        $(".unacceptable").fadeIn(100).delay(100).fadeOut(100);
+      };
+    }
+  });
+
+});
